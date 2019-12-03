774d8e65e0a9e7fae6d7c0fcaf0a8a68d1b3348f
guillermoLillo <guillermo.lillo@iesdoctorbalmis.com>
2019-12-03 21:45:19 +0100

    Solucionar add en ListBox

---
+++ ComandosyMenus/ComandosyMenus/ConvertidorHiden.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ComandosyMenus
+{
+    class ConvertidorHiden : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return "Visible";
+            else
+                return "Collapsed";
+        }   
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((String)value == "Visible")
+                return true;
+            else
+                return false;
+        }
+    }
+}
--- ComandosyMenus/ComandosyMenus/MainWindow.xaml.cs
+++ ComandosyMenus/ComandosyMenus/MainWindow.xaml.cs
@@ -20,15 +20,19 @@ namespace ComandosyMenus
     /// </summary>
     public partial class MainWindow : Window
     {
+        TextBlock itemListBox = null;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        //Añadir ----------------------------------------------------------------------------------------------------------------------
         private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             TextBlock item = new TextBlock();
-            item.Text = "Item añadido a las " + horaTextBlock.Text;
+                item.Text = "Item añadido a las " + horaTextBlock.Text;
+
             itemsListBox.Items.Add(item);
         }
 
@@ -40,22 +44,27 @@ namespace ComandosyMenus
                 e.CanExecute = false;
         }
 
+
+        //Copiar ----------------------------------------------------------------------------------------------------------------------
         private void CopyCommand_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            
+            itemListBox = (TextBlock)itemsListBox.SelectedItem;
         }
 
         private void CopyCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-            if (itemsListBox.Items.Count > 0)
+            if (itemsListBox.SelectedItem != null && itemsListBox.Items.Count > 0)
                 e.CanExecute = true;
             else
                 e.CanExecute = false;
         }
 
+
+        //Cortar ----------------------------------------------------------------------------------------------------------------------
         private void CutCommand_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-
+            itemListBox = (TextBlock)itemsListBox.SelectedItem;
+            itemsListBox.Items.Remove(itemsListBox.SelectedItem);
         }
 
         private void CutCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
@@ -66,40 +75,47 @@ namespace ComandosyMenus
                 e.CanExecute = false;
         }
 
+
+        //Pegar ----------------------------------------------------------------------------------------------------------------------
         private void PasteCommand_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-
+            itemsListBox.Items.Add(itemListBox);
         }
 
         private void PasteCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-            if (itemsListBox.SelectedItem != null)
+            if (itemListBox != null)
                 e.CanExecute = true;
             else
                 e.CanExecute = false;
         }
 
+
+        //Salida  ----------------------------------------------------------------------------------------------------------------------
         private void ExitCommand_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-
+            Close();
         }
 
         private void ExitCommand_CanExecuted(object sender, CanExecuteRoutedEventArgs e)
         {
-
+            e.CanExecute = true;
         }
 
+
+        //Eliminar Copia ----------------------------------------------------------------------------------------------------------------------
         private void DeleteCommand_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-
+            itemListBox = null;
         }
 
         private void DeleteCommand_CanExecuted(object sender, CanExecuteRoutedEventArgs e)
         {
-            if (itemsListBox.SelectedItem != null)
+            if (itemListBox != null)
                 e.CanExecute = true;
             else
                 e.CanExecute = false;
         }
+
     }
 }
